1ada89815279e62318a3be475c3225848ac31543
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-11 12:06:07 +0100

    fixed Repository and clean code (pairCoding)

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/Books.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/Books.java
@@ -21,51 +21,37 @@ public class Books {
     @Column(name = "title")
     private String title;
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public long getISBN() {
-        return ISBN;
-    }
-
-    public void setISBN(long ISBN) {
-        this.ISBN = ISBN;
-    }
-
     @Column(name = "genre")
     private String genre;
 
     @Column(name = "ISBN")
     private long ISBN;
 
+
+    public Books() {
+
+    }
     public Books(String author, String title, String genre, long ISBN){
         this.author = author;
         this.title = title;
         this.genre = genre;
         this.ISBN = ISBN;
     }
-    public Books() {
 
+    public String getAuthor() {
+        return author;
     }
+    public void setAuthor(String author) {this.author = author;}
+    public String getTitle() {return title;}
+
+    public void setTitle(String title) {this.title = title;}
+    public String getGenre() {return genre;}
+
+    public void setGenre(String genre) {this.genre = genre;}
+
+    public long getISBN() {return ISBN;}
+
+    public void setISBN(long ISBN) {this.ISBN = ISBN;}
+
+
 }
--- src/main/java/com/ROBOGUST/LibraryManagementSystem/BooksRepository.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/BooksRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BooksRepository extends JpaRepository {
+public interface BooksRepository extends JpaRepository <Books, Long>{
 
 }
