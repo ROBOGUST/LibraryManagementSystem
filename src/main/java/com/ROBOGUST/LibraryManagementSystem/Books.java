7a7849d2d575f179c3c18b016d6038d4478d2c02
Robin Sträng <91435503+Choppelito@users.noreply.github.com>
2023-01-11 12:26:56 +0100

    changed app.prop. auto=update to auto=none and refactored code with annotaions instead (paircoded)

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/Books.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/Books.java
@@ -2,7 +2,15 @@ package com.ROBOGUST.LibraryManagementSystem;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "books")
 public class Books {
@@ -11,47 +19,19 @@ public class Books {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    public long getId() {
-        return id;
-    }
-
     @Column(name = "author")
     private String author;
-
     @Column(name = "title")
     private String title;
-
     @Column(name = "genre")
     private String genre;
-
     @Column(name = "ISBN")
     private long ISBN;
 
-
-    public Books() {
-
-    }
     public Books(String author, String title, String genre, long ISBN){
         this.author = author;
         this.title = title;
         this.genre = genre;
         this.ISBN = ISBN;
     }
-
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {this.author = author;}
-    public String getTitle() {return title;}
-
-    public void setTitle(String title) {this.title = title;}
-    public String getGenre() {return genre;}
-
-    public void setGenre(String genre) {this.genre = genre;}
-
-    public long getISBN() {return ISBN;}
-
-    public void setISBN(long ISBN) {this.ISBN = ISBN;}
-
-
 }
