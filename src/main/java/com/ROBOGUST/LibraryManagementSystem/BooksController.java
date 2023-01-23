2e17f927408053245a8ee84b281fad2b8a04fce5
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-23 10:57:16 +0100

    implemented servicepart

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/BooksController.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/BooksController.java
@@ -1,15 +1,31 @@
 package com.ROBOGUST.LibraryManagementSystem;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.BooksServiceImpl;
+
+import java.util.List;
 
 @RestController
 public class BooksController {
+    private final BooksServiceImpl booksServiceImpl;
+
+
+    public BooksController(BooksServiceImpl booksServiceImpl) {
+        this.booksServiceImpl = booksServiceImpl;
+    }
 
     @GetMapping("/hello")
     public String Hello(){
 
         return "Hello";
+    }
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Books>>getAllBooks(){
+
+        return booksServiceImpl.getAllBooks;
     }
 
 }
--- src/main/java/service/BooksServiceImpl.java
+++ src/main/java/service/BooksServiceImpl.java
@@ -1,8 +1,22 @@
 package service;
 
+import com.ROBOGUST.LibraryManagementSystem.Books;
+import com.ROBOGUST.LibraryManagementSystem.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-@Service
-public class BooksServiceImpl {
+import java.util.List;
 
+@Service
+public class BooksServiceImpl implements BooksService {
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BooksServiceImpl(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+
+    public ResponseEntity<List<Books>> getAllBooks;
 }
