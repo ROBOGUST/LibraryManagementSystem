ee26fa72a982e7728a4240f15c323b2d7303b896
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-29 15:24:02 +0100

    changes in methods in user class

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/users/Users.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/users/Users.java
@@ -6,8 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -24,41 +26,42 @@ public class Users implements UserDetails {
     private String username;
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private Role role;
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
