6e92bb897b00af60a2d190410c0d0d7e5d8c6d17
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-29 15:39:18 +0100

    created userRepository and changed user class name

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/users/User.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/users/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class Users implements UserDetails {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
