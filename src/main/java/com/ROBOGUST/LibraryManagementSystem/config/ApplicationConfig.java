7d6b0a9dc23dff535bf5a16e19133c6beeec38f2
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-30 14:08:59 +0100

    ApplicationConfig class created

---
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.ROBOGUST.LibraryManagementSystem.config;
+
+import com.ROBOGUST.LibraryManagementSystem.users.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
