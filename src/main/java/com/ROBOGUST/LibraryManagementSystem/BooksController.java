73bd1d68063e3ff82f03d7a0cc71921719147108
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-31 15:23:15 +0100

    a few late changes in auth package

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/BooksController.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/BooksController.java
@@ -3,11 +3,12 @@ package com.ROBOGUST.LibraryManagementSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ROBOGUST.LibraryManagementSystem.service.BooksService;
 
 import java.util.List;
-
+@RequestMapping("/books")
 @RestController
 public class BooksController {
     private final BooksService booksService;
--- src/main/java/com/ROBOGUST/LibraryManagementSystem/config/JwtService.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/config/JwtService.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "";
+    private static final String SECRET_KEY = "77397A24432646294A404E635266556A586E327235753778214125442A472D4B";
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
