97f79d0ce6a8444e1910c1669757c9c575b235f3
HelenaOstlund <ostlund_helena@hotmail.com>
2023-02-01 14:30:14 +0100

    addViewController changed in WebConfig file

--- src/main/java/com/ROBOGUST/LibraryManagementSystem/WebConfig.java
+++ src/main/java/com/ROBOGUST/LibraryManagementSystem/WebConfig.java
@@ -14,5 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("logout");
+        registry.addViewController("/register").setViewName("register");
     }
 }
